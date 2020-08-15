fc9bdb96de31a173768e53bbd7f97197a1523065
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-08-15 14:13:32 -0500

    xss and delete post

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -59,12 +59,70 @@ describe('/articles/:article_id', function() {
                 .expect(200, expectedArticle)
         })
     })
+    context(`GIven an XSS attack article`, () => {
+        const maliciousArticle = { 
+            id: 911,
+            title: 'Naught naughty very naughty <script>('xss');</script>',
+            style: 'How-to',
+            content: `Bade image <img src="https://url.fake/does-not.exist" onerror`
+        }
+    })
     context('No articles in database', () => {
         it(`responds with 404`, () => {
             const articleId = 123456
             return supertest(app)
                 .get(`/articles/${articleId}`)
                 .expect(404, {error: {message: `Article doesn't exist`}})
+        })
+    })
+})
+
+describe('/articles POST', () => {
+    it('creates an article, responding with 201 and the new article', function(){
+        this.retries(3)
+        const newArticle = {
+            title: 'Test new article',
+            //style: 'Listicle',
+            content: 'Test new article content...'
+        }
+        return supertest(app)
+            .post('/articles')
+            .send(newArticle)
+            .expect(201)
+            .expect(res => {
+                expect (res.body.title).to.eql(newArticle.title)
+                //expect(res.body.style).to.eql(newArticle.style)
+                expect(res.body.content).to.eql(newArticle.content)
+                expect(res.body).to.have.property('id')
+                expect(res.headers.location).to.eql(`'articles/${res.body.id}`)
+                const expected = new Date().toLocaleString()
+                const actual = new Date(res.body.date_published).toLocaleString()
+                expect(actual).to.eql(expected)
+            })
+            .then(postRes => 
+                supertest(app)
+                    .get(`/articles/${postRes.body.id}`)
+                    .expect(postRes.body)    
+            )
+    })
+    const requiredFields = ['title', 'style', 'content']
+
+    requiredFields.forEach(field => {
+        const newArticle = {
+            title: 'Test new article',
+            style: 'Listicle',
+            content: 'Test new article content...'
+        }
+
+        it(`responds with 400 and an erro message when the '${field}' is missing`, () => {
+            delete newArticle[field]
+
+            return supertest(app)
+                .post('/articles')
+                .send(newArticles)
+                .expect(400, {
+                    error: {message: `Missing '${field}' in request body`}
+                })
         })
     })
 })
